5c9e0c8012677f04363e17507219f2e2a804ff0c
priyaganti <ganti.priya@gmail.com>
2018-03-28 17:31:52 -0700

    Header component with navbar and all school links

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/pre'>Pre Schools</Link></li>
+            <li><Link to='/elemntary'>Elemntary Schools</Link></li>
+            <li><Link to='/middle'>Middle Schools</li>
+            <li><Link to='/high'>High Schools</Link></li>
+            <li><Link to='/college'>Colleges</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
