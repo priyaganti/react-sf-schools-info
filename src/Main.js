a2b7ee7b6ca6833aec598c556097daece60928a3
priyaganti <ganti.priya@gmail.com>
2018-03-28 18:38:08 -0700

    add elementary and pre schol components

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Schools';
+import ElementarySchools from './ElementarySchools';
+import PreSchools from './PreSchools';
 import MiddleSchools from './MiddleSchools';
 import HighSchools from './HighSchools';
 
@@ -10,6 +12,8 @@ class Main extends React.Component {
       <main>
         <Switch>
           <Route exact path='/' component={Home}/>
+          <Route path='/elementary' component={ElementarySchools}/>
+          <Route path='/pre' component={PreSchools}/>
           <Route path='/middle' component={MiddleSchools}/>
           <Route path='/high' component={HighSchools}/>
         </Switch>
